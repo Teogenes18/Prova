b56d5f63a890680948ad2b22b22b076cef8d197c
Teogenes18 <143757049+Teogenes18@users.noreply.github.com>
2024-03-13 14:53:25 -0300

    Add files via upload

---
+++ vetor2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+int v[8],resultante[8],res[8],i,j,k;
+for(i=0;i<8;i++);
+printf("informe 8 valores %d/n",v[i]);
+for(i=0;i<8;i++)
+scanf("%d",&v[i]);
+for(i=0;i<8;i++)
+if(i>=1);
+scanf("%d",&resultante[i]);
+printf("os valores positivos sao %d",resultante[i]);
+if(i<=0);
+scanf("%d",&res[i]);
+printf("os valores negativos sao %d",res[1]);
+
+return 0;
+}
+
+
+
